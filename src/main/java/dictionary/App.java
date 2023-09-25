9d296324f6e966bf6f365fa9b7647de75976ee60
22021119_Man Thi Bich Phuong <124645869+phuongman@users.noreply.github.com>
2023-09-25 21:10:56 +0700

    fixx

--- src/main/java/dictionary/App.java
+++ src/main/java/dictionary/App.java
@@ -1,2 +1,8 @@
-package dictionary;public class App {
+package dictionary;
+
+public class App {
+    private enum choices {
+        Exit, Add, Remove, Update, Display, Lookup, Search, Game, ImportFromFile, ExportToFile;
+    }
+
 }
