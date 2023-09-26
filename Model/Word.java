7fccdede3ec8f25dbf08fb0ebc3420c98fb3f1cb
loilon504 <118033146+loilon504@users.noreply.github.com>
2023-09-26 17:02:11 +0700

    add Word.java

---
+++ Model/Word.java
@@ -0,0 +1,30 @@
+package dictionary.data;
+
+public class Word {
+    private final String wordTarget;
+    private String wordExplain;
+
+    /**
+     * Constructor.
+     * 
+     * @param _wordTarget  English word
+     * @param _wordExplain meaning of the word in Vietnamese
+     */
+    public Word(String _wordTarget, String _wordExplain) {
+        this.wordTarget = _wordTarget;
+        this.wordExplain = _wordExplain;
+    }
+
+    public String getWordTarget() {
+        return this.wordTarget;
+    }
+
+    public String getWordExplain() {
+        return this.wordExplain;
+    }
+
+    public void setWordExplain(String _wordExplain) {
+        this.wordExplain = _wordExplain;
+        return;
+    }
+}
