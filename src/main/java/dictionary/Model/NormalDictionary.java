e1fd0c1e259cf8b8f812419c97502f409abeb6ac
loilon504 <118033146+loilon504@users.noreply.github.com>
2023-09-27 14:48:32 +0700

    sửa lại normalDictionary.java

--- src/main/java/dictionary/Model/NormalDictionary.java
+++ src/main/java/dictionary/Model/NormalDictionary.java
@@ -10,29 +10,34 @@ public class NormalDictionary {
      * 
      * @param target  wordTarget of the word
      * @param meaning wordExplain of the word
+     * @return true if 'target' isn't in the dictionary (can add), otherwise false
      */
-    public void addWord(String target, String meaning) {
+    public boolean addWord(String target, String meaning) {
         for (int i = 0; i < words.size(); i++) {
             if (words.get(i).getWordTarget().equals(target)) {
-                return;
+                return false;
             }
         }
         Word w = new Word(target, meaning);
         words.add(w);
+        return true;
     }
 
     /**
      * remove a word in dictionary.
      * 
      * @param target wordTarget of the word
+     * @return true if 'target' is already in the dictionary (can remove), otherwise
+     *         false
      */
-    public void removeWord(String target) {
+    public boolean removeWord(String target) {
         for (int i = 0; i < words.size(); i++) {
             if (words.get(i).getWordTarget().equals(target)) {
                 words.remove(i);
-                return;
+                return true;
             }
         }
+        return false;
     }
 
     /**
@@ -40,18 +45,21 @@ public class NormalDictionary {
      * 
      * @param target  wordTarget of the word
      * @param meaning wordExplain of the word
+     * @return true if 'target' is already in the dictionary (can update), otherwise
+     *         false
      */
-    public void updateWordExplain(String target, String meaning) {
+    public boolean updateWordExplain(String target, String meaning) {
         for (int i = 0; i < words.size(); i++) {
             if (words.get(i).getWordTarget().equals(target)) {
                 words.get(i).setWordExplain(meaning);
-                return;
+                return true;
             }
         }
+        return false;
     }
 
     /**
-     * Help show all word from dictionary
+     * Help show all word from dictionary.
      * 
      * @return ArrayList words
      */
