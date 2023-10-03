390efdc393028582df6cdf5a934b3fcaed82db41
22021119_Man Thi Bich Phuong <124645869+phuongman@users.noreply.github.com>
2023-10-03 21:05:38 +0700

    updateWord.java

--- src/main/java/model/Word.java
+++ src/main/java/model/Word.java
@@ -1,4 +1,54 @@
 package model;
 
 public class Word {
+    /**
+     * @param word_target: từ mới
+     * @param word_explain: nghĩa của từ mới
+     */
+    private String word_target;
+    private String word_explain;
+
+    /**
+     * Constructor.
+     * @param word_target: từ mới
+     * @param word_explain: nghĩa của từ mới
+     */
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    /**
+     * return từ mới.
+     * @param word_target: từ mới
+     */
+    public String getWordTarget() {
+        return word_target;
+    }
+
+    /**
+     * return nghĩa của từ mới.
+     * @param word_explain: nghĩa của từ mới
+     */
+    public String getWordExplain() {
+        return word_explain;
+    }
+
+    /**
+     * set từ mới.
+     * @param word_target: từ mới
+     */
+    public void setWordTarget(String word_target) {
+        this.word_target = word_target;
+    }
+
+    /**
+     * Set nghĩa của từ mới.
+     * @param word_explain: nghĩa của từ mới
+     */
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
+
 }
