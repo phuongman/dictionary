11de70ef31ffffe1daa2986fc54ff9c7acaf5218
loilon504 <118033146+loilon504@users.noreply.github.com>
2023-10-30 21:46:04 +0700

    app

---
+++ src/main/java/ui/UiController.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UiController {
+
+}
